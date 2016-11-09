30e40ed1f22e81cc77ecfb3560e97eb170737487
harha <toni.s.h@netikka.fi>
2016-11-09 17:59:13 +0200

    Corrected minor thing with Main class.

--- src/main/java/io/github/harha/rest/Main.java
+++ src/main/java/io/github/harha/rest/Main.java
@@ -2,10 +2,9 @@ package io.github.harha.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Main extends WebMvcConfigurerAdapter {
+public class Main {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
